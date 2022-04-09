b155ab0a70721686ef5bb1d217ec2444a09cd6a4
Samuel Oliveira <11sosam11@gmail.com>
2022-04-09 11:58:59 -0500

    probably dont need all the packages i have but idk.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import Minter from './Minter.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <Minter></Minter>
+    </div>
+  );
+}
+
+export default App;
